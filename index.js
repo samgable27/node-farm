865e1436fe0455fb8ed8b2b8c24c6f7eaae91633
Sam <samarooniii@yahoo.com>
2023-06-27 15:14:32 -0400

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 ////////////////////////////////////////
@@ -34,6 +34,10 @@ const replaceTemplate = require("./modules/replaceTemplate");
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+
+console.log(slugs);
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
